a1bc3605ce24b87c196cb2097bbf6ec394e68578
Wagaye Tadele Kussa <wagayetadelekussa@gmail.com>
2025-06-04 13:08:04 +0200

    Update faceapi-module.js

--- faceapi-module.js
+++ faceapi-module.js
@@ -3,8 +3,8 @@ import * as faceapi from 'https://cdn.jsdelivr.net/npm/@vladmandic/face-api';
 
 // Preload models
 export async function loadFaceAPIModels() {
-    await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
-    await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+    await faceapi.nets.tinyFaceDetector.loadFromUri('/');
+    await faceapi.nets.faceLandmark68Net.loadFromUri('/');
     // await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
     console.log('FaceAPI models loaded successfully');
 }
